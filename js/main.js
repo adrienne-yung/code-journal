11fe905942adb8129c88f05ab141a0e06e323cd7
Adrienne Yung <adrienneyung64@gmail.com>
2021-12-20 13:16:30 -0800

    Created layout with css and index.html & input photo js.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var $image = document.querySelector('.placeholder-image');
+var $inputPhotoUrl = document.querySelector('#photo');
+
+$inputPhotoUrl.addEventListener('input', inputPhoto);
+function inputPhoto(event) {
+  var $inputPhotoUrlValue = $inputPhotoUrl.value;
+  $image.setAttribute('src', $inputPhotoUrlValue);
+}
